37dd379593656044e6ad06dbb641aec8c88494d2
lucas gil <lukemgl@outlook.com>
2021-12-15 17:48:22 -0300

    entrega desafio e-commerce

---
+++ src/components/NavBar/Navbar.js
@@ -0,0 +1,15 @@
+const NavBar = () => {
+  return (
+    <div className = "contenNav">
+      <ul>
+        <li id="brandNav">ALTEZZA NATURAL</li>
+        <li><a href=" ">INICIO</a></li>
+        <li><a href=" ">NUESTROS PRODUCTOS</a></li>
+        <li><a href=" ">CONTACTO</a></li>
+        <li><a href=" ">CARRITO</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default NavBar
